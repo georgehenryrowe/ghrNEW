a946393c9575b7f7a21dd1479e70b58703b125b6
George Rowe <georgehenryrowe@gmail.com>
2018-01-28 21:04:37 +0000

    Font, box tweaks

--- js/sketch.js
+++ js/sketch.js
@@ -1,40 +1,36 @@
+var time = millis();
+
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.parent('sketch-holder');
     colorMode(HSB,360,100,100,100);
+    // blendMode(BURN);
 }
 
 function draw() {
+
     background(30);
 
     //for generating a nice sine wave
-    var amplitude = 400;
-    var period = 2000;
-    var changingValue = amplitude * sin(TWO_PI * frameCount / period);
+    var amplitude = 200;
+    var period = 1000;
+    var changingValue = amplitude * cos(TWO_PI * frameCount / period);
     
     if (changingValue < 0) {
       changingValue = 0;
     }
   
-    //usefull drawing area stuff
-    var startGap = 200;
-    var sizeOfDrawableArea = width-(startGap*2);
-    //var sizeOfSquare = 300;
+    //useful drawing area stuff
+    var sizeOfThing = windowWidth*0.1;
     var numberOfThings = 1600;
-    var gapBetweenThings = 50;
+    var gapBetweenThings = sizeOfThing;
   
     //the golden ratio
     var goldenRatio = (1 + sqrt(5))/2;
   
     //********************* put cool stuff below here
   
-    //noFill();
     noStroke();
-    // lights();
-    //stroke(360);
-    //pushMatrix();
-    //translate(-500, -500);
-    //popMatrix();
     rotate(changingValue*0.01);
     translate(-width/2,-height/2);
     
@@ -44,18 +40,12 @@ function draw() {
         var maxDist = dist(0, 0, width*0.5, height*0.5);
         //var timeDelay = map(d, 0, maxDist, 1000, 2000);
         push();
-        //fill(d,60,80);
-        fill(d,100,100);
-        //rotate(changingValue*(d*0.00001));
+        fill(d/2,100,100);
         translate(x, y, changingValue*(d*0.01));
         rotateX(changingValue*(d*0.0001));
         rotateX(changingValue*(d*0.0001));
         rotateZ(changingValue*(d*0.0001));
-        //println(nf(var(d*0.1+(frameCount*0.2)),2));
-        //if (d*0.1+(frameCount*0.2) >= 360.0) {
-        //  exit();
-        //}
-        box(50);
+        box(sizeOfThing);
         pop();
       }
     }
