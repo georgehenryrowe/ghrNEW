b25923dc41f6d491a6b2fe8b3c4c5d00a8e776ee
George Rowe <georgehenryrowe@gmail.com>
2018-01-28 17:56:11 +0000

    Start getting some p5 in there

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('sketch-holder');
+    colorMode(HSB,360,100,100,100);
+}
+
+function draw() {
+  background(20);
+}
