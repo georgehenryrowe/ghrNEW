77f029a47e0417c16a9d24e8709fef319a339126
George Rowe <georgehenryrowe@gmail.com>
2018-01-31 21:38:55 +0000

    New patterns and changer

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@ var changingValue, sizeOfThing;
 var scrollMove = 1;
 var multiplierGap = 1;
 var multiplier = 1;
+var pattern = 1;
 
 
 function setup() {
@@ -27,7 +28,7 @@ function draw() {
 
     // var sizeOfThing = windowWidth*0.1;
     var numberOfThings = 2000;
-    var gapBetweenThings = sizeOfThing*multiplier;
+    var gapBetweenThings = sizeOfThing * multiplier;
 
     //the golden ratio
     var goldenRatio = (1 + sqrt(5)) / 2;
@@ -46,12 +47,29 @@ function draw() {
             var maxDist = dist(0, 0, width * 0.5, height * 0.5);
             //var timeDelay = map(d, 0, maxDist, 1000, 2000);
             push();
-            fill(d / 2 * map(mouseX, 0, width, 0.1, 2), 100, 100);
             translate(x, y, changingValue * (d * 0.01));
             rotateX(changingValue * (d * 0.0001));
             rotateY(changingValue * (d * 0.0002));
             rotateZ(changingValue * (d * 0.0001));
-            box(scrollPlusSize);
+            if (pattern == 1) {
+                fill(d / 2 * map(mouseX, 0, width, 0.1, 2), 100, 100);
+                box(scrollPlusSize);
+            } else if (pattern == 2) {
+                fill((x * 0.25) * map(mouseX, 0, width, 0.1, 2), 100, 100);
+                box(scrollPlusSize);
+            } else if (pattern == 3) {
+                noFill();
+                stroke(200, y * 0.25, map(mouseX, 0, width, 10, 100), 100, 100);
+                box(scrollPlusSize);
+            } else if (pattern == 4) {
+                fill(d / 2 * map(mouseX, 0, width, 0.1, 2), 100, 100);
+                rect(0, 0, scrollPlusSize, scrollPlusSize);
+                rect(-100, -100, scrollPlusSize, scrollPlusSize);
+                rect(100, 100, scrollPlusSize, scrollPlusSize);
+            } else if (pattern = 5) {
+                stroke((x * 0.25) * map(mouseX, 0, width, 0.1, 2), 100, 100)
+                line(0,0,width+scrollPlusSize,height+scrollPlusSize);
+            }
             pop();
         }
     }
@@ -67,20 +85,25 @@ function keyReleased() {
         save('myCanvas.png');
         console.log("Save");
     }
-    if (keyCode == 75){
+    if (keyCode == 75) {
         noLoop();
     }
-    if (keyCode == 76){
+    if (keyCode == 76) {
         loop();
     }
-    if (keyCode == 187){
-        multiplier-=0.1;
+    if (keyCode == 187) {
+        multiplier -= 0.1;
     }
-    if (keyCode == 189){
-        multiplier+=0.1;
+    if (keyCode == 189) {
+        multiplier += 0.1;
     }
-}
-
-function mousePressed() {
-
+    if (keyCode == 189) {
+        multiplier += 0.1;
+    }
+    if (keyCode == 67) {
+        pattern++;
+        if (pattern > 5) {
+            pattern = 1;
+        }
+    }
 }
